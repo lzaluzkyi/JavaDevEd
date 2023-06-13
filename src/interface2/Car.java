0a106757e020915ad425f661fafd7c914bcbf51e
LyubomirZaluzkiy <lzaluzkiy@gmail.com>
2023-06-13 20:31:21 +0300

    test

--- src/interface2/Car.java
+++ src/interface2/Car.java
@@ -10,6 +10,6 @@ public class Car implements Movable{
 
     @Override
     public void move() {
-        System.out.println("car drive with speed : " + speed);
+        System.out.println("car drive with speed hahaha: " + speed);
     }
 }
